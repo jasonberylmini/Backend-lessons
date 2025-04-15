a18ec1f5adead77c7be1ea9ae6aeaebfa95f0e7f
jasonberylmini <jasonberylmini@gmail.com>
2025-04-15 17:49:03 +0530

    Add file

--- src/helper/dbConnection.js
+++ src/helper/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = () => {
   mongoose
-    .connect(process.env.CONNECTION_STRING, { dbName: process.env.DB_NAME })
+    .connect(process.env.MONGODB_URI, { dbName: process.env.DB_NAME })
     .then(() => console.log("Connected!"))
     .catch((err) => {
       console.log("Err while connecting DB", err);
